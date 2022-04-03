216b43a713ca43c641c051fbf1481174e5bd5ab7
Zyhkss <55645482+Zyhkss@users.noreply.github.com>
2022-04-04 05:05:53 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -60,7 +60,7 @@ https://github.com/Zyhkss/Zenoxys
         if (new Date() * 1 - set.status > 1000) {
             let _uptime = process.uptime() * 1000
             let uptime = conn.clockString(_uptime)
-            await this.setStatus(`Aktif selama ${uptime} | Mode: ${set.self ? 'Private' : set.group ? 'Hanya Grup' : 'Publik'} | Zenoxysbot oleh ariffb`).catch(_ => _)
+            await this.setStatus(`Aktif selama ${uptime} | Mode: ${set.self ? 'Private' : set.group ? 'Hanya Grup' : 'Publik'} | Zenoxysbot oleh zyhkss`).catch(_ => _)
             set.status = new Date() * 1
         }
     }
